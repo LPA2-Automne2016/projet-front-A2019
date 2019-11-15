5e44e92ab7c42af06bb823d6c902a148998a52a5
GADOT Sebastien <sgadot@links.corp>
2019-11-15 11:07:11 +0100

    correction log

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
 exports.handler = async (event, context) => {
-    console.log(JSON.serialize(event),JSON.serialize(context) )
+    console.log(JSON.stringify(event),JSON.stringify(context) )
     return {
       statusCode: 200,
       body: "Hello, World"
