a7fa9846164afd72336ee341fad2bf4e4b94cb86
GADOT Sebastien <sgadot@links.corp>
2019-11-15 15:23:41 +0100

    ajout pwa register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ import { environment } from '../environments/environment';
     MatListModule,
     MatCardModule,
     HttpClientModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: true, registrationStrategy: 'registerImmediately' }),
   ],
   providers: [ActionCableService],
   bootstrap: [AppComponent]
